4bb13420656dd878826971af37d5ad2114abf500
Vivek205 <ichbinvivek@gmail.com>
2021-02-16 09:43:40 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /**
  * dotenv must be on the top of the entry file of the project
  */
@@ -7,10 +8,12 @@ import SnetSDK, { DefaultPaymentStrategy } from "snet-sdk";
 import service from "./proto/example_service_grpc_pb";
 import messages from "./proto/example_service_pb";
 import config from "./config";
+import cluster from "cluster";
 
-console.log("config", config);
+
 
 const sdk = new SnetSDK(config);
+
 var numbers = new messages.Numbers();
 
 const main = async (firstNumber, secondNumber) => {
@@ -18,29 +21,21 @@ const main = async (firstNumber, secondNumber) => {
   const serviceId = "test_freecall";
   const groupName = "default_group";
   const paymentStrategy = new DefaultPaymentStrategy(2);
-  const serviceClientOptionsPaidCall = {
-    // generated from marketplace : https://ropsten-dapp.singularitynet.io/servicedetails/org/6ce80f485dae487688c3a083688819bb/service/test_freecall
-    tokenToMakeFreeCall: "0X9acaaa2613ffd52e242695aaafabf1bb3619b0cca9068212958b5a2a3718d0ed0a008051b973e4af1d2049364d3734c9e1ea699922d933db8b258a5be3ad4ae51c".toUpperCase(),
-    // generated from marketplace : https://ropsten-dapp.singularitynet.io/servicedetails/org/6ce80f485dae487688c3a083688819bb/service/test_freecall,
-    tokenExpirationBlock: 9342373,
-    email: "freecallsdk@grr.la",
-    disableBlockchainOperations: false,
-    concurrency: true,
-  };
 
   const serviceClientOptionsFreeCall = {
-    email: "ichbinvivek@gmail.com",
-    // generated from marketplace : https://ropsten-dapp.singularitynet.io/servicedetails/org/6ce80f485dae487688c3a083688819bb/service/test_freecall
-    tokenToMakeFreeCall: "0xb725820105485aaf19a5bc96d3f5a89b57b005b6de62ac388cc85d24434c03501462e5350aded04756cf094f017f65c627278c0cb65f012021f3c49141eb5f211c",
-    // generated from marketplace : https://ropsten-dapp.singularitynet.io/servicedetails/org/6ce80f485dae487688c3a083688819bb/service/test_freecall,
-    tokenExpirationBlock: 9342672,
+    email: process.env.EMAIL,
+    // generated from marketplace 
+    tokenToMakeFreeCall: process.env.FREE_CALL_TOKEN.toUpperCase(),
+    // generated from marketplace 
+    tokenExpirationBlock: Number(process.env.TOKEN_EXPIRATION_BLOCK),
     disableBlockchainOperations: false,
     concurrency: true,
   };
 
   numbers.setA(firstNumber);
   numbers.setB(secondNumber);
-  console.log("numbers set");
+  console.log("config",config)
+  console.log("numbers set", serviceClientOptionsFreeCall);
 
   const closeConnection = () => {
     sdk.web3.currentProvider.connection && sdk.web3.currentProvider.connection.close();
